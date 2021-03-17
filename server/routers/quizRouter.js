3dfb03a51c1dcc8e9e225f6b1d729ee195f9436e
Sviut <jeleznov88@gmail.com>
2021-03-17 09:55:01 +0300

    add empty controllers

--- server/routers/quizRouter.js
+++ server/routers/quizRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const router = Router();
+const quizController = require('../controllers/quizController');
 
-router.post('/');
-router.get('/');
-router.get('/:id');
+router.post('/', quizController.create);
+router.get('/', quizController.getAll);
+router.get('/:id', quizController.getOne);
 
 module.exports = router;
