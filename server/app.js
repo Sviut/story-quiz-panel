ac13a61b4367a7c3b7e95b2fde7337a821340eb9
Sviut <jeleznov88@gmail.com>
2021-03-17 10:07:04 +0300

    add error middleware

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require('./routers');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+app.use(errorHandler);
 
 const start = async () => {
 	try {
